ddaedecbe74cbc0c9c0916555bc4acc99bf848c2
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2017-06-17 02:30:15 +1000

    (A mug of): dnsresolvd (Node.js): Start rewriting the daemon to resemble structure and functionality of its C counterpart.

---
+++ content/dev/misc/dnsresolvd/js/dnsresolvd.h
@@ -0,0 +1,81 @@
+/*
+ * content/dev/misc/dnsresolvd/js/dnsresolvd.h
+ * ============================================================================
+ * DNS Resolver Daemon (dnsresolvd). Version 0.1
+ * ============================================================================
+ * A Node.js-boosted daemon for performing DNS lookups.
+ * ============================================================================
+ * Copyright (C) 2017 Radislav (Radicchio) Golubtsov
+ *
+ * (See the LICENSE file at the top of the source tree.)
+ */
+
+"use strict";
+
+/**
+ * The helper class for the main daemon module.
+ * It aimed at accumulating such pieces like constants, globals, etc.,
+ * just like it behaves in its C counterpart. Yep, that's a little funny
+ * to make it named like in C, but definitely feasible nevertheless!))
+ */
+var __DNSRESOLVD_H = function() {
+    /* Helper constants. */
+    this._EXIT_FAILURE     =    1; //    Failing exit status.
+    this._EXIT_SUCCESS     =    0; // Successful exit status.
+    this._EMPTY_STRING     =   "";
+    this._ONE_SPACE_STRING =  " ";
+    this._COLON_SPACE_SEP  = ": ";
+    this._COMMA_SPACE_SEP  = ", ";
+    this._NEW_LINE         = "\n";
+
+    /* Common error messages. */
+    this._ERR_PREFIX                    = "Error";
+    this._ERR_PORT_MUST_BE_POSITIVE_INT = "%s: <port_number> must be "
+                                        + "a positive integer value, "
+                                        + "in the range 1024-49151.";
+    this._ERR_CANNOT_START_SERVER       = "%s: FATAL: Cannot start server "
+                                        + "for an unknown reason. Exiting...";
+    this._ERR_COULD_NOT_LOOKUP          = "Could not lookup hostname.";
+
+    /** Constant: Print this when there are no any args passed. */
+    this._ERR_MUST_BE_THE_ONLY_ARG = "%s: There must be exactly one arg "
+                                  + "passed: %u args found";
+
+    /** Constant: Print this usage info just after any inappropriate input. */
+    this._MSG_USAGE_TEMPLATE = "Usage: %s <port_number>";
+
+    /** Constant: The minimum port number allowed. */
+    this._MIN_PORT = 1024;
+
+    /** Constant: The maximum port number allowed. */
+    this._MAX_PORT = 49151;
+
+    /** Constant: The default port number to run the daemon on. */
+    this._DEF_PORT = 8765;
+
+    /* Common notification messages. */
+    this._MSG_SERVER_STARTED_1 = "Server started on port ";
+    this._MSG_SERVER_STARTED_2 = "=== Hit Ctrl+C to terminate it.";
+
+    /* HTTP response headers. */
+    this._HDR_CONTENT_TYPE  = "text/html; charset=UTF-8";
+    this._HDR_CACHE_CONTROL = "no-cache, no-store, must-revalidate";
+    this._HDR_EXPIRES       = "Thu, 01 Dec 1994 16:00:00 GMT";
+    this._HDR_PRAGMA        = "no-cache";
+
+    /* Daemon name, version, and copyright banners. */
+    this._DMN_NAME        = "DNS Resolver Daemon (dnsresolvd)";
+    this._DMN_DESCRIPTION = "Performs DNS lookups for the given hostname "
+                          + "passed in an HTTP request";
+    this._DMN_VERSION_S__ = "Version";
+    this._DMN_VERSION     = "0.1";
+    this._DMN_COPYRIGHT__ = "Copyright (C) 2017";
+    this._DMN_AUTHOR      = "Radislav Golubtsov <ragolubtsov@my.com>";
+
+    /** Constant: The default hostname to look up to. */
+    this._DEF_HOSTNAME = "archlinux.org";
+};
+
+module.exports = exports = __DNSRESOLVD_H;
+
+// vim:set nu:et:ts=4:sw=4:
--- content/dev/misc/dnsresolvd/js/dnsresolvd.js
+++ content/dev/misc/dnsresolvd/js/dnsresolvd.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
-/*
- * content/dev/misc/dnsresolvd/js/dnsresolvd.js
+/* content/dev/misc/dnsresolvd/js/dnsresolvd.js
  * ============================================================================
- * DNS Resolver Daemon. Version 0.1
+ * DNS Resolver Daemon (dnsresolvd). Version 0.1
  * ============================================================================
  * A Node.js-boosted daemon for performing DNS lookups.
  * ============================================================================
@@ -17,25 +16,13 @@ var http = require("http");
 var url  = require("url");
 var dns  = require("dns");
 
-/** The main and only class of the daemon. */
-//var DnsResolvD = function() {
-    /* Constant: The port number to run the daemon on. */
-    var _PORT_NUMBER = 8765;
+var __DNSRESOLVD_H = require("./dnsresolvd.h");
 
-    /* HTTP header properties. */
-    var _HTTP_200_OK  = 200;
-    var _CONTENT_TYPE = "text/html; charset=UTF-8";
+var aux = new __DNSRESOLVD_H();
 
-    /* Constant: The default hostname to look up to. */
-    var _HOSTNAME = "archlinux.org";
-
-    /* Helper constants. */
-    var _NEW_LINE               = "\n";
-    var _COLON_SPACE_SEP        = ": ";
-    var _MSG_SERVER_STARTED_1   = "Server started on port ";
-    var _MSG_SERVER_STARTED_2   = "=== Hit Ctrl+C to terminate it.";
-    var _ERROR_PREFIX           = "Error";
-    var _ERROR_COULD_NOT_LOOKUP = "Could not lookup hostname.";
+/** The daemon entry point. */
+var main = function() {
+    var ret = aux._EXIT_SUCCESS;
 
     // Creating the server and serving the request.
     http.createServer(function(req, resp) {
@@ -44,47 +31,51 @@ var dns  = require("dns");
         var hostname = query.h; // <== http://localhost:8765/?h=<hostname>
 
         if (!hostname) {
-            hostname = _HOSTNAME;
+            hostname = aux._DEF_HOSTNAME;
         }
 
         // Performing DNS lookup for the given hostname.
         dns.lookup(hostname, function(e, addr, ver) {
-            resp.writeHead(_HTTP_200_OK, {
-                "Content-Type" : _CONTENT_TYPE
+            resp.writeHead(200, {
+                "Content-Type" : aux._HDR_CONTENT_TYPE
             });
 
-            resp.write("<!DOCTYPE html>" + _NEW_LINE
-+ "<html lang=\"en-US\" dir=\"ltr\">"    + _NEW_LINE + "<head>"                  + _NEW_LINE
-+ "<meta http-equiv=\"Content-Type\"    content=\"text/html; charset=UTF-8\" />" + _NEW_LINE
-+ "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\" />"         + _NEW_LINE
-+ "<!-- No caching at all for:                                                                          -->"     + _NEW_LINE
-+ "<meta http-equiv=\"Cache-Control\"   content=\"no-cache, no-store, must-revalidate\" /> <!-- * HTTP/1.1. -->" + _NEW_LINE
-+ "<meta http-equiv=\"Expires\"         content=\"Thu, 01 Dec 1994 16:00:00 GMT\"       /> <!-- * Proxies.  -->" + _NEW_LINE
-+ "<meta http-equiv=\"Pragma\"          content=\"no-cache\"                            /> <!-- * HTTP/1.0. -->" + _NEW_LINE
-+ "<meta       name=\"viewport\"        content=\"width=device-width,initial-scale=1\" />"                       + _NEW_LINE
-            + "<title>DNS Resolver Daemon (dnsresolvd)</title>"  + _NEW_LINE
-            + "</head>" + _NEW_LINE + "<body id=\"dnsresolvd\">" + _NEW_LINE
-            + "<p>"     +  hostname + " ==&gt; ");
+            resp.write("<!DOCTYPE html>" + aux._NEW_LINE
++ "<html lang=\"en-US\" dir=\"ltr\">"    + aux._NEW_LINE + "<head>"              + aux._NEW_LINE
++ "<meta http-equiv=\"Content-Type\"    content=\"text/html; charset=UTF-8\" />" + aux._NEW_LINE
++ "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\" />"         + aux._NEW_LINE
++ "<!-- No caching at all for:                                                                          -->"     + aux._NEW_LINE
++ "<meta http-equiv=\"Cache-Control\"   content=\"no-cache, no-store, must-revalidate\" /> <!-- * HTTP/1.1. -->" + aux._NEW_LINE
++ "<meta http-equiv=\"Expires\"         content=\"Thu, 01 Dec 1994 16:00:00 GMT\"       /> <!-- * Proxies.  -->" + aux._NEW_LINE
++ "<meta http-equiv=\"Pragma\"          content=\"no-cache\"                            /> <!-- * HTTP/1.0. -->" + aux._NEW_LINE
++ "<meta       name=\"viewport\"        content=\"width=device-width,initial-scale=1\" />"                       + aux._NEW_LINE
+            + "<title>DNS Resolver Daemon (dnsresolvd)</title>"      + aux._NEW_LINE
+            + "</head>" + aux._NEW_LINE + "<body id=\"dnsresolvd\">" + aux._NEW_LINE
+            + "<p>"     +      hostname + " ==&gt; ");
 
             if (e) {
-                resp.write(_ERROR_PREFIX + _COLON_SPACE_SEP
-                         + _ERROR_COULD_NOT_LOOKUP);
+                resp.write(aux._ERR_PREFIX + aux._COLON_SPACE_SEP
+                         + aux._ERR_COULD_NOT_LOOKUP);
             } else {
                 resp.write(addr + " (IPv" + ver + ")");
             }
 
-            resp.write("</p>" + _NEW_LINE
-                  + "</body>" + _NEW_LINE
-                  + "</html>" + _NEW_LINE);
+            resp.write("</p>" + aux._NEW_LINE
+                  + "</body>" + aux._NEW_LINE
+                  + "</html>" + aux._NEW_LINE);
 
             resp.end();
         });
-    }).listen(_PORT_NUMBER);
+    }).listen(aux._DEF_PORT);
 
-    console.log(_MSG_SERVER_STARTED_1 + _PORT_NUMBER + _NEW_LINE
-              + _MSG_SERVER_STARTED_2);
-//};
+    return ret;
+};
 
-//module.exports = exports = DnsResolvD;
+var ret = main();
+
+if (ret === aux._EXIT_SUCCESS) {
+    console.log(aux._MSG_SERVER_STARTED_1 + aux._DEF_PORT + aux._NEW_LINE
+              + aux._MSG_SERVER_STARTED_2);
+}
 
 // vim:set nu:et:ts=4:sw=4:
