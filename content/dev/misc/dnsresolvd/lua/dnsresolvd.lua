e518919666bf8ad98052884091d67eedaa413f29
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2017-08-31 23:38:07 +1000

    (A mug of): dnsresolvd (Luvit:Lua): Add a rudimentary implementation of the helper Luvit module.

--- content/dev/misc/dnsresolvd/lua/dnsresolvd.lua
+++ content/dev/misc/dnsresolvd/lua/dnsresolvd.lua
@@ -10,9 +10,31 @@
  * (See the LICENSE file at the top of the source tree.)
 --]]
 
-local path = require("path")
-local http = require("http")
-local url  = require("url")
-local dns  = require("dns")
+--local path = require("path")
+--local http = require("http")
+--local url  = require("url")
+--local dns  = require("dns")
+
+local __DNSRESOLVD_H = require("dnsresolvd_h")
+
+-- The daemon entry point.
+function main(argc, argv)
+    local ret = __DNSRESOLVD_H._EXIT_SUCCESS
+
+    local daemon_name = ""--path.basename(argv[1])
+    local port_number = tonumber(argv, 10)--tonumber(argv[2], 10)
+
+    print("ret: " .. ret)
+
+    return ret
+end
+
+local argv = ""--process.argv
+local argc = argv.length
+
+-- Starting up the daemon.
+local ret = main(argc, argv)
+
+--process.exitCode = ret
 
 -- vim:set nu:et:ts=4:sw=4:
