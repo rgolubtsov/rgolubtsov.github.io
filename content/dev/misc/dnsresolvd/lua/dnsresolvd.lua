bdac7d79aae70dd85dba97f47a0b1289ff1598da
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2017-09-04 00:56:15 +1000

    (A mug of): dnsresolvd (Luvit:Lua): Continue porting from Node.js #1.

--- content/dev/misc/dnsresolvd/lua/dnsresolvd.lua
+++ content/dev/misc/dnsresolvd/lua/dnsresolvd.lua
@@ -10,32 +10,49 @@
  * (See the LICENSE file at the top of the source tree.)
 --]]
 
---local path = require("path")
+local path = require("path")
 --local http = require("http")
 --local url  = require("url")
 --local dns  = require("dns")
 
-local __dh = require("dnsresolvd_h")
+local aux = require("dnsresolvd_h")
+
+-- Helper function. Draws a horizontal separator banner.
+local _separator_draw = function(banner_text)
+    local i = banner_text:len()
+
+    while (i > 0) do
+        process.stdout:write('='); i = i - 1
+    end
+
+    print()
+end
 
 -- The daemon entry point.
-function main(argc, argv)
-    local ret = __dh._EXIT_SUCCESS
---  local ret = __dh._EXIT_FAILURE
+local main = function(argc, argv)
+    local ret = aux._EXIT_SUCCESS
 
-    local daemon_name = ""--path.basename(argv[1])
-    local port_number = tonumber(argv, 10)--tonumber(argv[2], 10)
+    local daemon_name = path.basename(argv[1])
+    local port_number = tonumber(argv[2], 10)
 
-    print("ret: " .. ret)
+    _separator_draw(aux._DMN_DESCRIPTION)
+
+    print(aux._DMN_NAME         .. aux._COMMA_SPACE_SEP .. aux._DMN_VERSION_S__
+       .. aux._ONE_SPACE_STRING .. aux._DMN_VERSION      .. aux._NEW_LINE
+       .. aux._DMN_DESCRIPTION                           .. aux._NEW_LINE
+       .. aux._DMN_COPYRIGHT__  .. aux._ONE_SPACE_STRING .. aux._DMN_AUTHOR)
+
+    _separator_draw(aux._DMN_DESCRIPTION)
 
     return ret
 end
 
-local argv = ""--process.argv
-local argc = argv.length
+local argv = process.argv
+local argc = #argv
 
 -- Starting up the daemon.
 local ret = main(argc, argv)
 
---process.exitCode = ret
+process.exitCode = ret
 
 -- vim:set nu:et:ts=4:sw=4:
