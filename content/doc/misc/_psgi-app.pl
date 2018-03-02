5af90169db60e42a96b1c317b467ebb966d8fa20
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2018-03-02 20:20:01 +1100

    Add PSGI app template to play with uWSGI server.

---
+++ content/doc/misc/_psgi-app.pl
@@ -0,0 +1,34 @@
+#
+# content/doc/misc/_psgi-app.pl
+# =============================================================================
+# PSGI application template.
+# Usage:
+#     $ uwsgi --http :8765 --http-modifier1 5 --psgi _psgi-app.pl
+#     $ curl localhost:8765
+# =============================================================================
+# See for ref.: https://uwsgi-docs.readthedocs.io/en/latest/PSGIquickstart.html
+#
+
+use strict;
+use warnings;
+use utf8;
+use v5.10;
+
+use constant TXT => "\nPSGI application template.\n";
+
+use constant {
+    HDR_CONTENT_TYPE => "text/plain",
+    RSC_HTTP_200_OK  => 200,
+};
+
+# The application entry point.
+my $app = sub {
+    my $env = shift();
+
+    my $resp = ["Content-Type" => HDR_CONTENT_TYPE];
+    my $_txt = [$env, TXT];
+
+    return [RSC_HTTP_200_OK, $resp, $_txt];
+};
+
+# vim:set nu et ts=4 sw=4:
