251fcdafcf1e555080e44ffffabef01c378f22a6
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2018-03-13 20:00:00 +1100

    PSGI/WSGI app templates: Add extra headers to the HTTP response.

--- content/doc/misc/_psgi-app.pl
+++ content/doc/misc/_psgi-app.pl
@@ -8,7 +8,7 @@
 #                          --http-modifier1  5           \
 #                          --psgi           _psgi-app.pl
 #
-#     $ curl localhost:8765; lynx localhost:8765
+#     $ curl localhost:8765; curl -XPOST localhost:8765; lynx localhost:8765
 # =============================================================================
 # See for ref.: https://uwsgi-docs.readthedocs.io/en/latest/PSGIquickstart.html
 #
@@ -24,24 +24,37 @@ use Data::Dumper;
 use constant APP_V => "PSGI application template";
 
 use constant {
-    HDR_CONTENT_TYPE => "application/json",
-    RSC_HTTP_200_OK  => 200,
+    HDR_CONTENT_TYPE  => "application/json",
+    HDR_CACHE_CONTROL => "no-cache, no-store, must-revalidate",
+    HDR_EXPIRES       => "Thu, 01 Dec 1994 16:00:00 GMT",
+    HDR_PRAGMA        => "no-cache",
+    RSC_HTTP_200_OK   => 200,
 };
 
 # The application entry point.
 my $app = sub {
     my $env = shift();
 
-    # JSONifying the response.
-    my $resp_buffer = encode_json({
-        envvars => Dumper($env),
-        appname => APP_V,
+    # Creating and JSONifying the response.
+    my $resp_buffer =  encode_json({
+        envvars     => Dumper($env),
+        appname     => APP_V,
     });
 
     # Adding headers to the response.
-    my $resp = ["Content-Type" => HDR_CONTENT_TYPE];
+    my $resp_headrs      =[
+        "Content-Type"   => HDR_CONTENT_TYPE,
+        "Content-Length" => length($resp_buffer),
+        "Cache-Control"  => HDR_CACHE_CONTROL,
+        "Expires"        => HDR_EXPIRES,
+        "Pragma"         => HDR_PRAGMA,
+    ];
 
-    return [RSC_HTTP_200_OK, $resp, [$resp_buffer]];
+    return [
+         RSC_HTTP_200_OK,
+         $resp_headrs,
+        [$resp_buffer]
+    ];
 };
 
 # vim:set nu et ts=4 sw=4:
--- content/doc/misc/_wsgi-app.py
+++ content/doc/misc/_wsgi-app.py
@@ -8,7 +8,7 @@
 #                          --callable   app         \
 #                          --wsgi-file _wsgi-app.py
 #
-#     $ curl localhost:8765; lynx localhost:8765
+#     $ curl localhost:8765; curl -XPOST localhost:8765; lynx localhost:8765
 # =============================================================================
 # See for ref.: https://uwsgi-docs.readthedocs.io/en/latest/WSGIquickstart.html
 #
@@ -19,23 +19,36 @@ ENV_N = "envvars"
 APP_N = "appname"
 APP_V = "WSGI application template"
 
-RSC_HTTP_200_OK    = "200"
-HDR_CONTENT_TYPE_N = "Content-Type"
-HDR_CONTENT_TYPE_V = "application/json"
+RSC_HTTP_200_OK      = "200"
+HDR_CONTENT_TYPE_N   = "Content-Type"
+HDR_CONTENT_TYPE_V   = "application/json"
+HDR_CONTENT_LENGTH_N = "Content-Length"
+HDR_CACHE_CONTROL_N  = "Cache-Control"
+HDR_CACHE_CONTROL_V  = "no-cache, no-store, must-revalidate"
+HDR_EXPIRES_N        = "Expires"
+HDR_EXPIRES_V        = "Thu, 01 Dec 1994 16:00:00 GMT"
+HDR_PRAGMA_N         = "Pragma"
+HDR_PRAGMA_V         = "no-cache"
 
 # The application entry point.
 def app(env, resp):
-    # JSONifying the response.
+    # Creating and JSONifying the response.
     resp_buffer = json.dumps({
         ENV_N : str(env),
         APP_N : APP_V
-    })
+    }).encode()
+
+    resp_headrs = [
+        (HDR_CONTENT_TYPE_N,   HDR_CONTENT_TYPE_V   ),
+        (HDR_CONTENT_LENGTH_N, str(len(resp_buffer))),
+        (HDR_CACHE_CONTROL_N,  HDR_CACHE_CONTROL_V  ),
+        (HDR_EXPIRES_N,        HDR_EXPIRES_V        ),
+        (HDR_PRAGMA_N,         HDR_PRAGMA_V         ),
+    ]
 
     # Adding headers to the response.
-    resp(RSC_HTTP_200_OK,
-       [(HDR_CONTENT_TYPE_N,
-         HDR_CONTENT_TYPE_V)])
+    resp(RSC_HTTP_200_OK, resp_headrs)
 
-    return resp_buffer.encode()
+    return [resp_buffer]
 
 # vim:set nu et ts=4 sw=4:
