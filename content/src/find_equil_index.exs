c83d0af39e213ef07eff7b4901ed90253f2d3719
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2018-11-02 16:20:00 +1100

    Add Elixir port of find_equil_index.js.

---
+++ content/src/find_equil_index.exs
@@ -0,0 +1,127 @@
+#!/usr/bin/env elixir
+# content/src/find_equil_index.exs
+# =============================================================================
+#     $ curl -O http://rgolubtsov.github.io/srcs/find_equil_index.exs && \
+#       chmod 700                                find_equil_index.exs && \
+#                                              ./find_equil_index.exs
+# =============================================================================
+# This is a demo script.
+#
+# A zero-indexed array A consisting of N integers is given. An equilibrium
+# index of this array is any integer P such that 0 <= P < N and the sum
+# of elements of lower indices is equal to the sum of elements of higher
+# indices, i.e.
+#
+#     A[0] + A[1] + ... + A[P-1] = A[P+1] + ... + A[N-2] + A[N-1].
+#
+# Sum of zero elements is assumed to be equal to 0. This can happen if P = 0
+# or if P = N-1.
+#
+# For example, consider the following array A consisting of N = 8 elements:
+#
+#     A[0] = -1
+#     A[1] =  3
+#     A[2] = -4
+#     A[3] =  5
+#     A[4] =  1
+#     A[5] = -6
+#     A[6] =  2
+#     A[7] =  1
+#
+# P = 1 is an equilibrium index of this array, because:
+#
+#     A[0] = -1 = A[2] + A[3] + A[4] + A[5] + A[6] + A[7]
+#
+# P = 3 is an equilibrium index of this array, because:
+#
+#     A[0] + A[1] + A[2] = -2 = A[4] + A[5] + A[6] + A[7]
+#
+# P = 7 is also an equilibrium index, because:
+#
+#     A[0] + A[1] + A[2] + A[3] + A[4] + A[5] + A[6] = 0
+#
+# and there are no elements with indices greater than 7.
+#
+# P = 8 is not an equilibrium index, because it does not fulfill
+# the condition 0 <= P < N.
+#
+# Write a function:
+#
+#     function solution(A);
+#
+# that, given a zero-indexed array A consisting of N integers, returns any
+# of its equilibrium indices. The function should return -1 if no equilibrium
+# index exists.
+#
+# For example, given array A shown above, the function may return 1, 3 or 7,
+# as explained above.
+#
+# Assume that:
+#
+#     N is an integer within the range [0..100,000];
+#     each element of array A is an integer within the range
+#     [-2,147,483,648..2,147,483,647].
+#
+# Complexity:
+#
+#     expected worst-case time complexity is O(N);
+#     expected worst-case space complexity is O(N), beyond input storage
+#     (not counting the storage required for input arguments).
+#
+# Elements of input arrays can be modified.
+# =============================================================================
+# Copyright (C) 2017-2018 Radislav (Radicchio) Golubtsov
+#
+# (See the LICENSE file at the top of the source tree.)
+#
+
+defmodule S do
+    @moduledoc "The solution function and helpers' container."
+
+    # Helper constants.
+    def _NEW_LINE,        do: "\n"
+    def _EQUIL_INDEX_MSG, do: "==> The equilibrium index of A is "
+
+    # Helper function. Calculates the sum of elements of lower  indices of A.
+    defp _sum_of_A_1(sum_of_A_1, a_) do
+        sum_of_A_1 + a_
+    end
+
+    # Helper function. Calculates the sum of elements of higher indices of A.
+    defp _sum_of_A_2(sum_of_A, sum_of_A_1, a_) do
+        sum_of_A_2 = sum_of_A - sum_of_A_1
+        sum_of_A_2 - a_
+    end
+
+    @doc """
+    The solution function.
+    """
+    def solution(a) do
+        # Calculating the complete sum of elements of A.
+        sum_of_A = :lists.sum(a)
+
+        # Searching for the equilibrium index of A.
+        try do
+            List.foldl(a, 0, fn(a_, i) ->
+                sum_of_A_1 = _sum_of_A_1(i, a_)
+
+                sum_of_A_2 = _sum_of_A_2(sum_of_A, sum_of_A_1, a_)
+
+                if (sum_of_A_1 === sum_of_A_2), do: throw(i),
+                                              else: i + 1
+            end)
+
+            -1 # Returning -1 if there's no such an equilibrium index exists.
+        catch
+            i -> i
+        end
+    end
+end
+
+a = [-1, 3, -4, 5, 1, -6, 2, 1]
+
+i = S.solution(a)
+
+IO.puts(S._NEW_LINE <> S._EQUIL_INDEX_MSG <> to_string(i) <> S._NEW_LINE)
+
+# vim:set nu et ts=4 sw=4:
