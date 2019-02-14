2d631f7106fde842a24afdaeae8d02f4bd4db51f
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2019-02-14 20:30:00 +1100

    Claim Clojure and Groovy scripts run well on Ubuntu Server.

--- content/src/replace_txt_chunks.clj
+++ content/src/replace_txt_chunks.clj
@@ -8,7 +8,8 @@
 ; =============================================================================
 ; This is a demo script. It has to be run in the Clojure (JVM) runtime
 ; environment. Tested and known to run exactly the same way on modern versions
-; of OpenBSD/amd64, Arch Linux / Arch Linux 32 operating systems.
+; of OpenBSD/amd64, Ubuntu Server LTS x86-64, Arch Linux / Arch Linux 32
+; operating systems.
 ;
 ; Create a function that will take a String value as the first parameter,
 ; a Number value as the second parameter, and a String value as the third one.
