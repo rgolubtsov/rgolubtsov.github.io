440fe5ea9746196932c527eabec73b8fc19421dc
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2021-09-04 16:27:00 +1000

    Magistracy Admission Decree: Add corresponding sequel scripts.

---
+++ partials/mojolicious/abi/master/create---table-bteu_magistracy_admission_decree_users.sql
@@ -0,0 +1,16 @@
+--
+-- create---table-bteu_magistracy_admission_decree_users.sql
+-- ============================================================================
+-- Magistracy Admission Decree - Users Realm.
+-- Приказ о зачислении в магистратуру.
+-- ============================================================================
+-- Written by Radislav (Radicchio) Golubtsov, 2021
+--
+
+create table bteu_magistracy_admission_decree_users (id            smallserial primary key,
+                                                     username      character   varying(30) not null,
+                                                     given_name    character   varying(30) not null,
+                                                     password      character   varying(7)  not null,
+                                                     studying_form smallint);
+
+-- vim:set nu et ts=4 sw=4:
