05377bc74077d0cfb6e08e71f5ab36f31fe0a985
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2021-09-04 16:30:00 +1000

    Stage 1 Entrance Exams Results: Add corresponding sequel scripts.

---
+++ partials/mojolicious/abi/results/populate-table-2-bteu_stage1_entrance_exams_results_users.sql
@@ -0,0 +1,18 @@
+--
+-- populate-table-2-bteu_stage1_entrance_exams_results_users.sql
+-- ============================================================================
+-- Stage 1 Entrance Exams Results.
+-- Результаты вступительных испытаний.
+-- ============================================================================
+-- Written by Radislav (Radicchio) Golubtsov, 2021
+--
+
+insert into bteu_stage1_entrance_exams_results_users (username, given_name, password, studying_form_id, subject1_id, date1_id, subject2_id, date2_id) values ('FamilyName1', 'GivenName1', '1234567', 1, 2, 2, 1, 1);
+insert into bteu_stage1_entrance_exams_results_users (username, given_name, password, studying_form_id, subject1_id, date1_id, subject2_id, date2_id) values ('FamilyName2', 'GivenName2', '2345678', 2, 6, 2, 1, 1);
+insert into bteu_stage1_entrance_exams_results_users (username, given_name, password, studying_form_id, subject1_id, date1_id, subject2_id, date2_id) values ('FamilyName3', 'GivenName3', '3456789', 2, 8, 2, 1, 1);
+insert into bteu_stage1_entrance_exams_results_users (username, given_name, password, studying_form_id, subject1_id, date1_id, subject2_id, date2_id) values ('FamilyName4', 'GivenName4', '4567890', 2, 7, 2, 4, 1);
+insert into bteu_stage1_entrance_exams_results_users (username, given_name, password, studying_form_id, subject1_id, date1_id, subject2_id, date2_id) values ('FamilyName5', 'GivenName5', '5678901', 2, 3, 1, 5, 2);
+-- .....
+-- 271..
+
+-- vim:set nu et ts=4 sw=4:
