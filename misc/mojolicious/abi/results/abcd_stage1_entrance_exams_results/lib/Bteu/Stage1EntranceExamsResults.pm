bc7a6183d6aa1eda0eeb85d0ec2ab6dc038c26e6
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2022-04-27 01:30:00 +1000

    Add forgotten inner `lib/`s due to previously misconfigured Git ignorables.

---
+++ misc/mojolicious/abi/results/abcd_stage1_entrance_exams_results/lib/Bteu/Stage1EntranceExamsResults.pm
@@ -0,0 +1,94 @@
+#
+# lib/Bteu/Stage1EntranceExamsResults.pm
+# =============================================================================
+# Stage 1 Entrance Exams Results.
+# Результаты вступительных испытаний.
+# =============================================================================
+# Written by Radislav (Radicchio) Golubtsov, 2021
+#
+
+package Bteu::Stage1EntranceExamsResults;
+
+use Mojo::Base "Mojolicious", -signatures;
+
+use Try::Tiny;
+use DBI;
+
+# Helper constants.
+use constant _COLON_SPACE_SEP => ": ";
+
+# Common error messages.
+use constant {
+    _ERROR_PREFIX        => "Error",
+    _ERROR_NO_DB_CONNECT => "Could not connect to the database.",
+};
+
+use constant {
+    DATABASE => "<db_name>",
+    HOSTNAME => "localhost",
+};
+
+use constant {
+    DSN      => "dbi:Pg:database=" . DATABASE . ";host=" . HOSTNAME,
+    USERNAME => "<dbusername>",
+    PASSWORD => "<dbpassword>",
+};
+
+use constant {
+    RAISE_ERROR_DBI_ATTR => 1,
+    AUTO_COMMIT_DBI_ATTR => 0,
+};
+
+##
+# Starts up the app.
+#
+# @param {Object} self - The object instance of the class.
+#
+sub startup($self) {
+    # Loading the configuration.
+    my $config = $self->plugin("NotYAMLConfig");
+
+    # Configuring the application.
+    $self->secrets($config->{secrets});
+
+    my $_secrets = $self->secrets();
+
+    $self->defaults(session_key => $_secrets->[0]);
+
+    my $dbh;
+
+    my %attr = (
+        RaiseError => RAISE_ERROR_DBI_ATTR,
+        AutoCommit => AUTO_COMMIT_DBI_ATTR,
+    );
+
+    # Trying to connect to the database.
+    try {
+        $dbh = DBI->connect(DSN, USERNAME, PASSWORD, \%attr);
+    } catch {
+        say(__PACKAGE__ . _COLON_SPACE_SEP . _ERROR_PREFIX
+                        . _COLON_SPACE_SEP . $_);
+    };
+
+    if ($dbh) {
+        # Storing the database handle object in the application object.
+        $self->defaults(dbh => $dbh);
+    } else {
+        say(__PACKAGE__ . _COLON_SPACE_SEP . _ERROR_PREFIX
+                        . _COLON_SPACE_SEP . _ERROR_NO_DB_CONNECT);
+    }
+
+    # Getting the router object.
+    my $router = $self->routes();
+
+    $router->any ("/"                 )->to("Login#show_login"   );
+    $router->get ("/login"            )->to("Login#show_login"   );
+    $router->post("/login"            )->to("Login#do_login"     );
+    $router->get ("/results"          )->to("Results#go_results" );
+    $router->get ("/results/rezultaty")->to("Results#get_results");
+    $router->post("/logout"           )->to("Login#do_logout"    );
+}
+
+1;
+
+# vim:set nu et ts=4 sw=4:
