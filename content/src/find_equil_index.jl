e2387690aa27a4e1c23884e6c32ccf6555b93ae4
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2023-05-12 14:10:30 +0300

    Assorted sources: Julia port of `find_equil_index.js`: Make script download and launch command to be easily copyable without further modifications.

--- content/src/find_equil_index.jl
+++ content/src/find_equil_index.jl
@@ -3,8 +3,8 @@
  * ============================================================================
  * Usage:
  *   $ curl -sO http://rgolubtsov.github.io/srcs/find_equil_index.jl && \
- *     chmod 700                                 find_equil_index.jl && \
- *                                             ./find_equil_index.jl; echo $?
+       chmod 700                                 find_equil_index.jl && \
+                                               ./find_equil_index.jl; echo $?
  * ============================================================================
  * This is a demo script. It has to be run as a **script** in the Julia runtime
  * environment. Tested and known to run exactly the same way on modern versions
