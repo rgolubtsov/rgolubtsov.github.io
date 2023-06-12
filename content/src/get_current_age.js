29c29019676474798b3aaf1189c0fb6fa0e385b1
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2023-06-13 01:00:00 +0300

    Assorted sources: `get_current_age.js`: Add shebang line, enrich and normalize top block comment.

--- content/src/get_current_age.js
+++ content/src/get_current_age.js
@@ -1,6 +1,23 @@
-// get_current_age.js
-// Copyright (C) 2023 Radislav (Radicchio) Golubtsov
-// (See the LICENSE file at the top of the source tree.)
+#!/usr/bin/env node
+/* content/src/get_current_age.js
+ * ============================================================================
+ * Usage:
+ *   $ curl -sO http://rgolubtsov.github.io/srcs/get_current_age.js && \
+       chmod 700                                 get_current_age.js && \
+                                               ./get_current_age.js; echo $?
+ * ============================================================================
+ * This is a demo script. It has to be run in the Node.js runtime environment.
+ * Tested and known to run exactly the same way on modern versions
+ * of Ubuntu Server LTS x86-64 and Arch Linux operating systems.
+ *
+ * The script contains a function that calculates the current age
+ * based on the date of birth, given in args. It then simply returns
+ * the age calculated - nothing special.
+ * ============================================================================
+ * Copyright (C) 2023 Radislav (Radicchio) Golubtsov
+ *
+ * (See the LICENSE file at the top of the source tree.)
+ */
 
 "use strict";
 
