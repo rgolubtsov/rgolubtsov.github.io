79436fe20cac2045debd5a23b13fdcf7cf3a597a
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2023-08-14 00:20:00 +0300

    Assorted sources: Add Clojure port of `get_current_age.lfe`.

---
+++ content/src/get-current-age/get_current_age.clj
@@ -0,0 +1,40 @@
+#!/usr/bin/env clojure
+; content/src/get-current-age/get_current_age.clj
+; =============================================================================
+; Usage:
+;   $ SRCS=http://rgolubtsov.github.io/srcs; \
+;       curl  -sO ${SRCS}/get-current-age/get_current_age.clj   && \
+;       chmod 700 get_current_age.clj;  ./get_current_age.clj;  echo $?
+; =============================================================================
+; This is a demo script. It has to be run in the Clojure (JVM) runtime
+; environment. Tested and known to run exactly the same way on modern versions
+; of Ubuntu Server LTS x86-64 and Arch Linux operating systems.
+;
+; The script contains a function that calculates the current age
+; based on the date of birth, given in args. It then simply returns
+; the age calculated - nothing special.
+; =============================================================================
+; Copyright (C) 2023 Radislav (Radicchio) Golubtsov
+;
+; (See the LICENSE file at the top of the source tree.)
+;
+
+(defn get-age
+    "Calculates the current age based on the date of birth."
+    [YY-OF-BIRTH MM-OF-BIRTH DD-OF-BIRTH]
+
+    (let [date  (java.time.LocalDate/now)]
+    (let [year  (.getYear       date)]
+    (let [month (.getMonthValue date)]
+    (let [day   (.getDayOfMonth date)]
+
+    (let [age (- year YY-OF-BIRTH)]
+
+    (if (< month MM-OF-BIRTH) (- age 1)
+        (if (== month MM-OF-BIRTH) (if (< day DD-OF-BIRTH) (- age 1) age) age)
+    ))))))
+)
+
+(println (get-age 1977 6 27)) ; <== 1977-06-27.
+
+; vim:set nu et ts=4 sw=4:
