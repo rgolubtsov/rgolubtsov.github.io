9fd55098878ea8ef5c97807767295bdaf54a32b5
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2025-03-25 18:10:00 +0300

    Assorted sources: JavaScript: Fix stylistics - do conform to the others.

--- content/src/get-current-age/get_current_age.js
+++ content/src/get-current-age/get_current_age.js
@@ -7,8 +7,8 @@
        chmod 700 get_current_age.js;   ./get_current_age.js;  echo $?
  * ============================================================================
  * This is a demo script. It has to be run in the Node.js runtime environment.
- * Tested and known to run exactly the same way on modern versions
- * of OpenBSD/amd64, Ubuntu Server LTS x86-64, Arch Linux operating systems.
+ * Tested and known to run exactly the same way on modern versions of
+ * OpenBSD/amd64, Ubuntu Server LTS x86-64, and Arch Linux operating systems.
  *
  * The script contains a function that calculates the current age
  * based on the date of birth, given in args. It then simply returns
